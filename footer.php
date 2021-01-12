d28ca6e4f243476d0fc6fbf62c67a16f74298814
fengxianqi <609587734@qq.com>
2021-01-12 09:51:40 +0800

    fix: 备案链接

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 </div><!-- end #body -->
 
 <footer id="footer" class="footer" role="contentinfo">
-  <em><a href="http://www.miitbeian.gov.cn"  target="_blank"><?php echo $this->options->beian;?></a>
+  <em><a href="https://beian.miit.gov.cn"  target="_blank"><?php echo $this->options->beian;?></a>
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
     <?php _e('由 <a href="//www.typecho.org">Typecho</a> 强力驱动.'); ?>.
   </em>
